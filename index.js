ce215fdbfb1a44fcbc56f138fb7a9d5b70b3992d
m7mark <m7mark@yandex.ru>
2021-09-16 23:28:58 +0300

    MongoDB connection

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config()
 
-app.listen(5000, () => {
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("mdb success"))
+    .catch((err) => console.log(err))
+
+app.listen(process.env.PORT || 5000, () => {
     console.log("Starting backend");
 })
