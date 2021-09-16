679cbb52d571fa2306b454736352097ac4ca596a
m7mark <m7mark@yandex.ru>
2021-09-17 00:46:08 +0300

    auth route & user schema db

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express()
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+const authRoute = require("./routes/auth")
+
 dotenv.config()
 
 mongoose
@@ -9,6 +11,8 @@ mongoose
     .then(() => console.log("mdb success"))
     .catch((err) => console.log(err))
 
+app.use(express.json())
+app.use("/api/auth", authRoute)
 app.listen(process.env.PORT || 5000, () => {
     console.log("Starting backend");
 })
