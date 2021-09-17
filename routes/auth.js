7d5dc445308c71d2a8d89b3305a6d0b68f06c40c
m7mark <m7mark@yandex.ru>
2021-09-17 13:16:29 +0300

    Login users

--- routes/auth.js
+++ routes/auth.js
@@ -18,4 +18,25 @@ router.post("/register", async (req, res) => {
     } catch (err) { res.status(500).json(err) }
 })
 
+//Login
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({
+            username: req.body.username
+        });
+        if (!user) { return res.status(401).json("Wrong credentials!") }
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+        const OrigPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+        if (OrigPassword !== req.body.password) {
+            return res.status(401).json("Wrong credentials!")
+        }
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch (err) { res.status(500).json(err) }
+});
+
 module.exports = router
