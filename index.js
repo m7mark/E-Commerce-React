a2699db58712a7801c4245f7777430032e2728b1
m7mark <m7mark@yandex.ru>
2021-09-17 22:59:35 +0300

    Stripe payment done!

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const dotenv = require("dotenv")
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/user")
 const productRoute = require("./routes/product")
-const productCart = require("./routes/cart")
-const productOrder = require("./routes/order")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
+const stripeRoute = require("./routes/stripe")
+const cors = require("cors")
 
 dotenv.config()
 
@@ -16,11 +18,13 @@ mongoose
     .catch((err) => console.log(err))
 
 app.use(express.json())
+app.use(cors())
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/products", productRoute)
-app.use("/api/cart", productCart)
-app.use("/api/orders", productOrder)
+app.use("/api/cart", cartRoute)
+app.use("/api/orders", orderRoute)
+app.use("/api/checkout", stripeRoute)
 app.listen(process.env.PORT || 5000, () => {
     console.log("Starting backend");
 })
