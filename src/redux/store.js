b9d2cbf26dace841fa44368b8ecc3e3ddecd76d6
m7mark <m7mark@yandex.ru>
2021-10-03 15:41:14 +0300

    redux-persist store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,43 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import {
+  combineReducers,
+  configureStore,
+} from '@reduxjs/toolkit';
 
 import cartSlice from './cartSlice';
 import userSlice from './userSlice';
 
-export default configureStore({
-  reducer: {
-    cart: cartSlice,
-    user: userSlice,
-  }
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const rootReducer = combineReducers({
+  cart: cartSlice,
+  user: userSlice,
 })
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export let persistor = persistStore(store)
