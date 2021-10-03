d2f5a9690f231ad1687797b056a4e3518f999f8b
m7mark <m7mark@yandex.ru>
2021-10-03 16:34:22 +0300

    Refactor spaces

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const cors = require("cors")
 dotenv.config()
 
 mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => console.log("mdb success"))
-    .catch((err) => console.log(err))
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("mdb success"))
+  .catch((err) => console.log(err))
 
 app.use(express.json())
 app.use(cors())
@@ -26,5 +26,5 @@ app.use("/api/cart", cartRoute)
 app.use("/api/orders", orderRoute)
 app.use("/api/checkout", stripeRoute)
 app.listen(process.env.PORT || 5000, () => {
-    console.log("Starting backend");
+  console.log("Starting backend");
 })
