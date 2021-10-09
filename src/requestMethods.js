9969752242b28b0cb5341e7902fc21789a985cef
m7mark <m7mark@yandex.ru>
2021-10-10 01:53:50 +0300

    Dynamic token

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL ='http://localhost:5000/api/'
-const TOKEN ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNDQ4NzdlMmI4NzcwNjk2MTA4OTc4MCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzMzA1MTAwNSwiZXhwIjoxNjMzMzEwMjA1fQ.kgSEInTrE-0EnMp-XBgYgZfZWDuDj9xOAsddB2K4vQk'
+const BASE_URL = 'http://localhost:5000/api/'
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser.accessToken
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
@@ -9,5 +9,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: {token:`Bearer ${TOKEN}`}
+  headers: { token: `Bearer ${TOKEN}` }
 })
