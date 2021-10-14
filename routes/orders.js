35b19fc18452ae9289c980a8d832fb27240ae606
m7mark <m7mark@yandex.ru>
2021-10-15 00:25:45 +0300

    query 'pid'

--- routes/orders.js
+++ routes/orders.js
@@ -50,12 +50,18 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
 
 //GET MONTHLY INCOME
 router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+  const productId = req.query.pid
   const date = new Date()
   const lastMonth = new Date(date.setMonth(date.getMonth() - 1))
   const previousMonth = new Date(date.setMonth(lastMonth.getMonth() - 1))
   try {
     const income = await Order.aggregate([
-      { $match: { createdAt: { $gte: previousMonth } } },
+      {
+        $match: {
+          createdAt: { $gte: previousMonth },
+          ...(productId && { products: { $elemMatch: { productId: productId } }, })
+        }
+      },
       {
         $project: {
           month: { $month: "$createdAt" },
@@ -70,6 +76,7 @@ router.get("/income", verifyTokenAndAdmin, async (req, res) => {
       }
     ])
     res.status(200).json(income);
+
   } catch (err) { res.status(500), json(err) }
 })
 
