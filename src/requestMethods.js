d54ac6e6d76344142f7d5f88651c0d0b92404ca8
m7mark <m7mark@yandex.ru>
2021-10-20 02:03:54 +0300

    req methods

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:5000/api/'
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser.accessToken
+
+const currentUserRedux = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser
+
+let TOKEN
+currentUserRedux
+  ? TOKEN = currentUserRedux.accessToken
+  : TOKEN = null
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
@@ -9,5 +15,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  headers: { token: `Bearer ${TOKEN}` }
+  headers: { token: `Bearer ${TOKEN}` },
 })
