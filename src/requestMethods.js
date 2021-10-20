76625e59dd2612ce7b440916f900219be4ffe457
m7mark <m7mark@yandex.ru>
2021-10-20 20:14:01 +0300

    requestMethods

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api/'
-
-const currentUserRedux = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser
+const BASE_URL = process.env.REACT_APP_SERVER_API
 
 let TOKEN
-currentUserRedux
-  ? TOKEN = currentUserRedux.accessToken
-  : TOKEN = null
+if (localStorage.getItem('persist:root')) {
+  const currentUserRedux = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser
+  currentUserRedux
+    ? TOKEN = currentUserRedux.accessToken
+    : TOKEN = null
+}
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
